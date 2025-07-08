a8c453488a9942b7e2be4c7798bd04dce5141261
phamanhtuan-coder <0306221391@caothang.edu.vn>
2025-07-08 15:05:33 +0700

    Update SIGNAL_PIN definition to use GPIO2
    Changed SIGNAL_PIN from 8 to 2 to correctly reference GPIO2 for ESP-01, ensuring proper pin configuration for communication with the UNO.

--- Door/ESP01_Slave_Door_Normal_7/ESP01_Slave_Door_Normal_7.ino
+++ Door/ESP01_Slave_Door_Normal_7/ESP01_Slave_Door_Normal_7.ino
@@ -11,7 +11,7 @@ uint8_t gatewayMAC[6] = {0x48, 0x3F, 0xDA, 0x1F, 0x4A, 0xA6};
 String DEVICE_SERIAL = "SERL27JUN2501JYR2RKVS2P6XBVF1P2E";
 
 // ESP-01 PIN CONFIGURATION
-#define SIGNAL_PIN 8  // GPIO2 - sends to UNO Receive
+#define SIGNAL_PIN 2  // GPIO2 - sends to UNO Receive
 
 // ESP-NOW message structure
 struct CompactMessage {
